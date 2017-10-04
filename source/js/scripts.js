b46daf9188b15e0a466b4a30e25c0e1d364ae9b1
Nick Pavelchuk <koksua13@gmail.com>
2017-10-05 02:23:07 +0300

    Fixes by comments. Without filter of PIO

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -114,59 +114,96 @@ $( ".add-template" ).click(function() {
 });
 
 // Multy checkboxes
-$(function () {
-    var liSet_Id;
-    $("#filter-points li").each( function (i){
-        var liSet_Id = i+1;
-        $("ul", this).attr("id", "categorySubItem_"+liSet_Id);
-        $(".check-all-group input[type='checkbox']", this).attr("id", "checkedAllBox_"+liSet_Id);
-    });
+// $(function () {
+//     var liSet_Id;
+//     $("#filter-points li").each( function (i){
+//         var liSet_Id = i+1;
+//         $("ul", this).attr("id", "categorySubItem_"+liSet_Id);
+//         $(".check-all-group input[type='checkbox']", this).attr("id", "checkedAllBox_"+liSet_Id);
+//     });
+//
+//     $(".check-all-group input[type=checkbox]").change(function () {
+//         var checkedAllId = $(this).attr("id");
+//         var numbIdsElem = checkedAllId.split('_').pop();
+//
+//         if($(this).is(":checked")){
+//             $(this).parent("label").removeClass('-multi');
+//             $("#categorySubItem_"+numbIdsElem+" .checkbox-control input[type=checkbox]").each(function(){
+//                 $(this).prop('checked', 'checked');
+//                 $(this).parent("label").removeClass('-multi');
+//             });
+//         }
+//         else{
+//             $(this).parent("label").removeClass("-multi");
+//             $("#categorySubItem_"+numbIdsElem+" .checkbox-control input[type=checkbox]").each(function(){
+//                 $(this).prop('checked', '');
+//                 $(this).parent("label").removeClass('-multi');
+//             });
+//         }
+//     });
+//
+//
+//     $('.checkbox-control input[type=checkbox]').change (function (){
+//     	if ($(this).hasClass("check-all-group")) {
+//             return false;
+//         }
+//         var idParentUl = $(this).closest("ul").attr("id");
+//         var numbIdsElem = idParentUl.split('_').pop();
+//
+//         var countChBoxChec = $("#"+idParentUl+" > li .checkbox-control input[type=checkbox]:checked").length;
+//         var countChBoxNotChec = $("#"+idParentUl+" > li .checkbox-control input[type=checkbox]:not(:checked)").length;
+//
+//         // if($(this).is(":checked")){
+//         //     $(this).prop('checked', 'checked');
+//         // }
+//         // else{
+//         //     $(this).prop('checked', '');
+//         // }
+//         if (countChBoxChec != 0 && countChBoxNotChec == 0) {
+//             $("#checkedAllBox_"+numbIdsElem).prop('checked', 'checked').parent("label").removeClass('-multi');
+//         }
+//         else if (countChBoxChec != 0 && countChBoxNotChec != 0) {
+//             $("#checkedAllBox_"+numbIdsElem).prop('checked', '').parent("label").addClass("-multi");
+//         }
+//         else{
+//             $("#checkedAllBox_"+numbIdsElem).prop('checked', '').parent("label").removeClass('-multi');
+//         }
+//     });
+// });
 
-    $(".check-all-group input[type=checkbox]").change(function () {
-        var checkedAllId = $(this).attr("id");
-        var numbIdsElem = checkedAllId.split('_').pop();
-
-        if($(this).is(":checked")){
-            $(this).parent("label").removeClass('-multi');
-            $("#categorySubItem_"+numbIdsElem+" .checkbox-control input[type=checkbox]").each(function(){
-                $(this).prop('checked', 'checked');
-                $(this).parent("label").removeClass('-multi');
-            });
-        }
-        else{
-            $(this).parent("label").removeClass("-multi");
-            $("#categorySubItem_"+numbIdsElem+" .checkbox-control input[type=checkbox]").each(function(){
-                $(this).prop('checked', '');
-                $(this).parent("label").removeClass('-multi');
-            });
-        }
-    });
-
-
-    $('.checkbox-control input[type=checkbox]').change (function (){
-    	if ($(this).hasClass("check-all-group")) {
-            return false;
-        }
-        var idParentUl = $(this).closest("ul").attr("id");
-        var numbIdsElem = idParentUl.split('_').pop();
-
-        var countChBoxChec = $("#"+idParentUl+" > li .checkbox-control input[type=checkbox]:checked").length;
-        var countChBoxNotChec = $("#"+idParentUl+" > li .checkbox-control input[type=checkbox]:not(:checked)").length;
-
-        // if($(this).is(":checked")){
-        //     $(this).prop('checked', 'checked');
-        // }
-        // else{
-        //     $(this).prop('checked', '');
-        // }
-        if (countChBoxChec != 0 && countChBoxNotChec == 0) {
-            $("#checkedAllBox_"+numbIdsElem).prop('checked', 'checked').parent("label").removeClass('-multi');
-        }
-        else if (countChBoxChec != 0 && countChBoxNotChec != 0) {
-            $("#checkedAllBox_"+numbIdsElem).prop('checked', '').parent("label").addClass("-multi");
-        }
-        else{
-            $("#checkedAllBox_"+numbIdsElem).prop('checked', '').parent("label").removeClass('-multi');
-        }
-    });
+// click function for showing the searchbox in the Header
+$('.poi-block--group-title').on('click', function(){
+    $(this).toggleClass('-closed').blur();
+    $(this).next('.poi-block--points-group').slideToggle();
+    return false;
 });
+
+// Filter by point of interests
+// $(document).ready(function () {
+//     /* highlight matches text */
+//     var highlight = function (string) {
+//         $("#search-poi-list label.label-control.match").each(function () {
+//             var matchStart = $(this).text().toLowerCase().indexOf("" + string.toLowerCase() + "");
+//             var matchEnd = matchStart + string.length - 1;
+//             var beforeMatch = $(this).text().slice(0, matchStart);
+//             var matchText = $(this).text().slice(matchStart, matchEnd + 1);
+//             var afterMatch = $(this).text().slice(matchEnd + 1);
+//             $(this).html(beforeMatch + "<span>" + matchText + "</span>" + afterMatch);
+//         });
+//     };
+//
+//     /* filter interests */
+//     $("#search-poi-input").on("keyup", function () {
+//         if (this.value.length >= 0) {
+//             $("#search-poi-list label.label-control").filter(function () {
+//                 return $(this).text().toLowerCase().indexOf($("#search-poi-input").val().toLowerCase()) != -1;
+//             }).addClass("match");
+//
+//             //$("#search-poi-list .label-control:not('.match')").parent('li').hide();
+//             highlight(this.value);
+//         }
+//         else {
+//             $("#search-poi-list .label-control.match").removeClass("match").parent('li').show();
+//         }
+//     });
+// });
